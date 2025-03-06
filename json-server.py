5838bbebb35c4f828abc92f10dbbad55355b05ab
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-06 17:25:23 -0500

    saving changes before pull

--- json-server.py
+++ json-server.py
@@ -30,7 +30,7 @@ class JSONServer(HandleRequests):
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
         
         elif url["requested_resource"] == "categories":
-            response_body = list_categories(url)
+            response_body = list_categories()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
         
         elif url["requested_resource"] == "users":
@@ -38,7 +38,7 @@ class JSONServer(HandleRequests):
                 response_body = retrieve_user(url["pk"])
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
-            response_body = list_users(url)
+            response_body = list_users()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
             
