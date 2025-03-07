7d8a313c94d86b84ef831a00ec9bbc2914190abb
John Bailey Williams <johnbaileywilliams96@gmail.com>
2025-03-07 12:04:06 -0600

    added url to list_categories

--- views/category_view.py
+++ views/category_view.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 
-def list_categories():
+def list_categories(url):
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
